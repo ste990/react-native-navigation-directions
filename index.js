b53c3dc558dbb5facb228d2a41b8f0ea82b74ddc
Daly <stephen.daly@vships.com>
2021-10-21 23:29:49 +0100

    Added functionality for Addresses

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ import isValidCoordinates from 'is-valid-coordinates';
 
 const PLATFORM = Platform.OS;
 
-export const OpenMapDirections = (frmCoord = null, toCoord, transportType) => new Promise((resolve, reject) => {
-	let _toCoord;
-	const _frmCoord = _checkParameters(frmCoord) !== null ? `?saddr=${_checkParameters(frmCoord)}` : '';
-	if (_checkParameters(toCoord) !== null) {
-		_toCoord = (_frmCoord.length == 0 ? '?' : '&') + `daddr=${_checkParameters(toCoord)}`
-	} else {
-		throw new Error('You need to pass a valid endpoint(number)')
+export const OpenMapDirections = (startPoint = null, endPoint, transportType) => new Promise((resolve, reject) => {
+	var _to;
+	var _frm = _checkCoordParameter(startPoint) !== null ? `?saddr=${_checkCoordParameter(startPoint)}` : '';
+	var _frm = _checkAddrParameter(startPoint) !== null ? `?saddr=${_checkCoordParameter(startPoint)}` : '';
+	if (_checkCoordParameter(endPoint) !== null) {
+		_to = (_frm.length == 0 ? '?' : '&') + `daddr=${_checkCoordParameter(endPoint)}`
+	} 
+	  else if (_checkAddrParameter(endPoint) !== null)  {
+		  _to = (_frm.length == 0 ? '?' : '&') + `daddr=${_checkAddrParameter(endPoint)}`
+	  }
+	  else {
+		throw new Error('You need to pass a valid endpoint(coordinate or address in Data URL format)')
 	};
 	const _transportType = _checkTransportParameter(transportType) !== null ? `&dirflg=${_checkTransportParameter(transportType)}` : '';
-	const url = `${PLATFORM === 'ios' ? `https://maps.apple.com/` : 'https://maps.google.com/'}${_frmCoord}${_toCoord}${_transportType}`;
+	const url = `${PLATFORM === 'ios' ? `https://maps.apple.com/` : 'https://maps.google.com/'}${_frm}${_to}${_transportType}`;
 	_openApp(url).then(result => { resolve(result) });
 });
 
@@ -28,11 +33,21 @@ const _openApp = (url) => new Promise((resolve, reject) => {
 		})
 });
 
-const _checkParameters = (param) => {
+const _checkCoordParameter = (param) => {
 	if (param === null || param === undefined || typeof param.latitude === 'string' || typeof param.longitude === 'string') { return null; }
 
 	if (isValidCoordinates.longitude(param.longitude) && isValidCoordinates.latitude(param.latitude)) {
 		return `${param.latitude},${param.longitude}`
+	}
+
+	return null;
+}
+
+const _checkAddrParameter = (param) => {
+	if (param === null || param === undefined || typeof param.Address !== 'string' || param.Address.includes(' ')  ) { return null; }
+
+	if (typeof param.Address === 'string') {
+		return `${param.Address}`
 	}
 
 	return null;
